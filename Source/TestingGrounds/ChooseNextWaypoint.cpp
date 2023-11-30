af9df14620f88227628e380d155adef6adf68549
Beau <beau@lioncode.nl>
2023-11-30 23:03:30 +0100

    continued working on c++

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,9 +2,12 @@
 
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
-EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory)
-{
-    UE_LOG(LogTemp, Warning, TEXT("overridden"));
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory) {
+    UBlackboardComponent* BlackBoardComponent = OwnerComp.GetBlackboardComponent();
+    int Index = BlackBoardComponent->GetValueAsInt(IndexKey.SelectedKeyName);
+    UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
     return EBTNodeResult::Succeeded;
 }
